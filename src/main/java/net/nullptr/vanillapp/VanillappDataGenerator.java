e9e01fa93b4960fbb38cdf2e3f49d933b43390df
Samith Shetty <samithshetty0@gmail.com>
2024-05-17 11:17:44 -0400

    Initialized Fabric Project

---
+++ src/main/java/net/nullptr/vanillapp/VanillappDataGenerator.java
@@ -0,0 +1,11 @@
+package net.nullptr.vanillapp;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VanillappDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
