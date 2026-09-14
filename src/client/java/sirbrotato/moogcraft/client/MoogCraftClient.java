de38fac1a6d9b0eced4e81862a4b5de7fe1d657c
Samith Shetty <71335825+samithShetty@users.noreply.github.com>
2026-09-13 20:28:25 -0400

    Initialized Default Fabric Project

---
+++ src/client/java/sirbrotato/moogcraft/client/MoogCraftClient.java
@@ -0,0 +1,10 @@
+package sirbrotato.moogcraft.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoogCraftClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
